2cb335607ce1cb8e212fb9b4a0857c081bd7db96
Ahmad Allabadi <allabada@mcmaster.ca>
2025-11-10 13:00:55 -0500

    Added projets src

---
+++ src/lib/projects.ts
@@ -0,0 +1,10 @@
+export const projects = [
+    {
+        title: "MacMarketPlace",
+        description: "Campus marketplaace for McMaster Students.",
+        tech: ["React", "TailwindCSS", "Firebase", "Algolia", "GCP"],
+        highlight: "200+ users within the first month of launch.",
+        link: "https://macmarketplace.ca",
+        image: ""
+    },
+]
